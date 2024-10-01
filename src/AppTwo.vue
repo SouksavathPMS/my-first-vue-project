6c35f91a628e52ceb8eb6ad1f848c5ee71cc52f5
Your Name <you@example.com>
2024-10-01 16:27:04 +0700

    Setting up vue-router

--- src/AppTwo.vue
+++ src/AppTwo.vue
@@ -1,15 +1,11 @@
 <script setup>
 import MyNavbar from "./components/MyNavbar.vue";
-import HeroBar from "./components/HeroBar.vue";
-import HomeCard from "./components/HomeCard.vue";
-import JobListings from "./components/JobListings.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <MyNavbar />
-  <HeroBar />
-  <HomeCard />
-  <JobListings :limit="3" :showButton="true" />
+  <RouterView />
 </template>
 
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HeroBar from "@/components/HeroBar.vue";
+import HomeCard from "@/components/HomeCard.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
+<template>
+  <HeroBar />
+  <HomeCard />
+  <JobListings :limit="3" :showButton="true" />
+</template>
+
+
