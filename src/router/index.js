25a04063a77302aa670e852028e75507322437d0
Your Name <you@example.com>
2024-10-02 16:33:00 +0700

    CRUD functionality on job

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import JobListings from '../components/JobListings.vue';
+import AddJobView from '../components/AddJobView.vue';
 import NotFoundView from '../components/NotFoundView.vue';
 import JobView from '../components/JobView.vue';
+import EditJobView from '../components/EditJobView.vue';
 import AddJob from '../views/AddJob.vue';
 
 const router = createRouter({
@@ -27,6 +29,16 @@ const router = createRouter({
       path: '/job/:id',
       name: 'addJobs',
       component: JobView,
+    },
+    {
+      path: '/jobs/add',
+      name: 'add-job',
+      component: AddJobView,
+    },
+    {
+      path: '/jobs/edit/:id',
+      name: 'edit-job',
+      component: EditJobView,
     },
     {
       path: '/:catchAll(.*)',
